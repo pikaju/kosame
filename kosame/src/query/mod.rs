516d61fab9cb58448746ff42daab9ca0f2ac6686
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2025-10-16 18:08:03 +0200

    Simplify exec functions

--- kosame/src/query/mod.rs
+++ kosame/src/query/mod.rs
@@ -10,14 +10,14 @@ pub use node::*;
 pub use order_by::*;
 pub use runner::*;
 
-use pollster::FutureExt;
-
 use crate::{
     Error,
     driver::Connection,
     expr::Expr,
+    params::Params,
     schema::{Column, Relation, Table},
 };
+use pollster::FutureExt;
 
 pub trait Query {
     type Params: std::fmt::Debug;
@@ -34,11 +34,11 @@ pub trait Query {
     fn exec<'c, C>(
         &self,
         connection: &mut C,
-        runner: &mut (impl crate::query::Runner + ?Sized),
-    ) -> impl Future<Output = Result<Vec<<Self as crate::query::Query>::Row>, Error<C>>>
+        runner: &mut (impl Runner + ?Sized),
+    ) -> impl Future<Output = Result<Vec<Self::Row>, Error<C>>>
     where
         C: Connection,
-        Self::Params: crate::params::Params<C::Params<'c>>,
+        Self::Params: Params<C::Params<'c>>,
         for<'b> Self::Row: From<&'b C::Row>,
     {
         async { runner.run(connection, self).await }
@@ -47,11 +47,11 @@ pub trait Query {
     fn exec_one<'c, C>(
         &self,
         connection: &mut C,
-        runner: &mut (impl crate::query::Runner + ?Sized),
-    ) -> impl Future<Output = Result<<Self as crate::query::Query>::Row, Error<C>>>
+        runner: &mut (impl Runner + ?Sized),
+    ) -> impl Future<Output = Result<Self::Row, Error<C>>>
     where
         C: Connection,
-        Self::Params: crate::params::Params<C::Params<'c>>,
+        Self::Params: Params<C::Params<'c>>,
         for<'b> Self::Row: From<&'b C::Row>,
     {
         async {
@@ -64,11 +64,11 @@ pub trait Query {
     fn exec_opt<'c, C>(
         &self,
         connection: &mut C,
-        runner: &mut (impl crate::query::Runner + ?Sized),
-    ) -> impl Future<Output = Result<Option<<Self as crate::query::Query>::Row>, Error<C>>>
+        runner: &mut (impl Runner + ?Sized),
+    ) -> impl Future<Output = Result<Option<Self::Row>, Error<C>>>
     where
         C: Connection,
-        Self::Params: crate::params::Params<C::Params<'c>>,
+        Self::Params: Params<C::Params<'c>>,
         for<'b> Self::Row: From<&'b C::Row>,
     {
         async {
@@ -86,11 +86,11 @@ pub trait Query {
     fn exec_sync<'c, C>(
         &self,
         connection: &mut C,
-        runner: &mut (impl crate::query::Runner + ?Sized),
-    ) -> Result<Vec<<Self as crate::query::Query>::Row>, Error<C>>
+        runner: &mut (impl Runner + ?Sized),
+    ) -> Result<Vec<Self::Row>, Error<C>>
     where
         C: Connection,
-        Self::Params: crate::params::Params<C::Params<'c>>,
+        Self::Params: Params<C::Params<'c>>,
         for<'b> Self::Row: From<&'b C::Row>,
     {
         self.exec(connection, runner).block_on()
@@ -99,11 +99,11 @@ pub trait Query {
     fn exec_one_sync<'c, C>(
         &self,
         connection: &mut C,
-        runner: &mut (impl crate::query::Runner + ?Sized),
-    ) -> Result<<Self as crate::query::Query>::Row, Error<C>>
+        runner: &mut (impl Runner + ?Sized),
+    ) -> Result<Self::Row, Error<C>>
     where
         C: Connection,
-        Self::Params: crate::params::Params<C::Params<'c>>,
+        Self::Params: Params<C::Params<'c>>,
         for<'b> Self::Row: From<&'b C::Row>,
     {
         self.exec_one(connection, runner).block_on()
@@ -112,11 +112,11 @@ pub trait Query {
     fn exec_opt_sync<'c, C>(
         &self,
         connection: &mut C,
-        runner: &mut (impl crate::query::Runner + ?Sized),
-    ) -> Result<Option<<Self as crate::query::Query>::Row>, Error<C>>
+        runner: &mut (impl Runner + ?Sized),
+    ) -> Result<Option<Self::Row>, Error<C>>
     where
         C: Connection,
-        Self::Params: crate::params::Params<C::Params<'c>>,
+        Self::Params: Params<C::Params<'c>>,
         for<'b> Self::Row: From<&'b C::Row>,
     {
         self.exec_opt(connection, runner).block_on()
