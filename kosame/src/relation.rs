4da62064db4bed632eddd5ec502a50c61716b0be
Julien Scholz <8947616+pikaju@users.noreply.github.com>
2025-10-22 20:42:38 +0200

    Add One relation

--- kosame/src/relation.rs
+++ kosame/src/relation.rs
@@ -3,10 +3,39 @@ use std::ops::{Deref, DerefMut};
 #[derive(Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize))]
 #[cfg_attr(feature = "serde-full", derive(serde::Deserialize))]
+pub struct One<T>(T);
+
+impl<T> One<T> {
+    pub fn new(inner: T) -> Self {
+        Self(inner)
+    }
+
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+}
+
+impl<T> Deref for One<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for One<T> {
+    fn deref_mut(&mut self) -> &mut <Self as Deref>::Target {
+        &mut self.0
+    }
+}
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize))]
+#[cfg_attr(feature = "serde-full", derive(serde::Deserialize))]
 pub struct ZeroOrOne<T>(Option<T>);
 
 impl<T> ZeroOrOne<T> {
-    pub(crate) fn new(inner: Option<T>) -> Self {
+    pub fn new(inner: Option<T>) -> Self {
         Self(inner)
     }
 
@@ -34,6 +63,16 @@ impl<T> DerefMut for ZeroOrOne<T> {
 #[cfg_attr(feature = "serde-full", derive(serde::Deserialize))]
 pub struct Many<T>(Vec<T>);
 
+impl<T> Many<T> {
+    pub fn new(inner: Vec<T>) -> Self {
+        Self(inner)
+    }
+
+    pub fn into_vec(self) -> Vec<T> {
+        self.0
+    }
+}
+
 impl<T> Deref for Many<T> {
     type Target = Vec<T>;
 
@@ -45,15 +84,5 @@ impl<T> Deref for Many<T> {
 impl<T> DerefMut for Many<T> {
     fn deref_mut(&mut self) -> &mut <Self as Deref>::Target {
         &mut self.0
-    }
-}
-
-impl<T> Many<T> {
-    pub(crate) fn new(inner: Vec<T>) -> Self {
-        Self(inner)
-    }
-
-    pub fn into_vec(self) -> Vec<T> {
-        self.0
     }
 }
